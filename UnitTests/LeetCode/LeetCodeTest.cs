cfc41553fba2d79260f50b1fcab9af625aa2aaec
xiaochun-z <madcaden@outlook.com>
2025-08-08 21:26:21 +0800

    added 021

---
+++ UnitTests/LeetCode/LeetCodeTest.cs
@@ -0,0 +1,29 @@
+namespace LeetCodeTests;
+
+[TestClass]
+public sealed class L_020_ValidParenthese
+{
+    [TestMethod]
+    public void Test_ValidParenthese()
+    {
+        var solution = new LeetCode.L020_ValidParenthese.Solution();
+        Assert.IsTrue(solution.IsValid("(6,5)-(14,6)"));
+        Assert.IsTrue(solution.IsValid("()"));
+        Assert.IsTrue(solution.IsValid("()[]{}"));
+        Assert.IsFalse(solution.IsValid("(]"));
+        Assert.IsTrue(solution.IsValid("([])"));
+        Assert.IsFalse(solution.IsValid("([)]"));
+    }
+
+    [TestMethod]
+    public void Test_MergeSortedList()
+    {
+        var solution = new LeetCode.L021_MergeTwoSortedList.Solution();
+        var list1 = new LeetCode.L021_MergeTwoSortedList.ListNode(1, new LeetCode.L021_MergeTwoSortedList.ListNode(2, new LeetCode.L021_MergeTwoSortedList.ListNode(4)));
+        var list2 = new LeetCode.L021_MergeTwoSortedList.ListNode(1, new LeetCode.L021_MergeTwoSortedList.ListNode(3, new LeetCode.L021_MergeTwoSortedList.ListNode(4)));
+
+        var result = solution.MergeTwoLists(list1, list2);
+
+        CollectionAssert.AreEqual(new[] { 1, 1, 2, 3, 4, 4 }, result.ToArray());
+    }
+}

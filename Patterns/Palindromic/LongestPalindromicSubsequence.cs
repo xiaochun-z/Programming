7761992928184376cd6bc121f8581c8ea6484905
xiaochun-z <madcaden@outlook.com>
2025-10-09 08:38:24 +0800

    added template for lps

---
+++ Patterns/Palindromic/LongestPalindromicSubsequence.cs
@@ -0,0 +1,38 @@
+namespace Programming.Patterns.Palindromic.LongestPalindromicSubsequence;
+
+/*
+Problem Statement
+Given a sequence, find the length of its Longest Palindromic Subsequence (LPS). In a palindromic subsequence, elements read the same backward and forward.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+
+Input: "abdbca"
+Output: 5
+Explanation: LPS is "abdba".
+Example 2:
+
+Input: = "cddpd"
+Output: 3
+Explanation: LPS is "ddd".
+Example 3:
+
+Input: = "pqr"
+Output: 1
+Explanation: LPS could be "p", "q" or "r".
+Constraints:
+
+1 <= st.length <= 1000
+sy consists only of lowercase English letters.
+*/
+
+using System;
+public class Solution
+{
+    public int findLPSLength(string st)
+    {
+        // TODO: Write your code here
+        return -1;
+    }
+}
